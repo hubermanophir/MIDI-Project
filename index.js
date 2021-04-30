61da0883e5370d29f9000524e53298c3c99669a8
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-04-30 23:07:25 +0300

    added build

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const express = require("express");
 const app = require("./app");
 const PORT = 8080;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build"));
+});
 
 app.listen(PORT, () => {
   console.log(`server live on port ${PORT}`);

addf6ff182a6195cc4a5de4116d5598bf4b7a8cc
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-05-01 18:12:43 +0300

    added scene function

--- MainFunction.js
+++ MainFunction.js
@@ -8,4 +8,11 @@ function main(presetNumber, scene) {
   });
 }
 
-module.exports = { main };
+function scene(scene) {
+  WebMidi.enable().then(() => {
+    let axeFxIn = WebMidi.getOutputById("2- AXE-FX II MIDI Out").channels[1];
+    axeFxIn.sendControlChange(34, scene - 1);
+  });
+}
+
+module.exports = { main, scene };
